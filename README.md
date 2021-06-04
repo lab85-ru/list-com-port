4ef357e5124bb3aa1e3911bada77f0af8903804a
sgd <sgot@inbox.ru>
2021-06-04 14:09:39 +0300

    UPDATE: icon - file for this app.

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 [https://lab85.ru description App](https://lab85.ru/index.php/laboratoriya-stati/22-utilita-dlya-opredeleniya-kolichestva-com-portov-na-pk-os-windows)
 
 <img src="./img/list-com-port.jpg" width="475">
+<img src="./img/GIF-ka.gif" width="532">
--- list-com-port/Form1.Designer.vb
+++ list-com-port/Form1.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Button1 = New System.Windows.Forms.Button
         Me.tbLog = New System.Windows.Forms.TextBox
         Me.Button2 = New System.Windows.Forms.Button
@@ -64,6 +65,7 @@ Partial Class Form1
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.tbLog)
         Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Наличие СОМ портов."
